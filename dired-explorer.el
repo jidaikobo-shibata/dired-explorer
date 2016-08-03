9fd733baae082f8a5d60fff79341b469bc3538f0
shibata <shibata@jidaikobo.com>
2016-08-03 17:45:45 +0900

    add licence text.

--- dired-explorer.el
+++ dired-explorer.el
@@ -3,7 +3,22 @@
 ;; Original2: http://www.bookshelf.jp/soft/meadow_25.html#SEC286
 ;; Introduce and Supervise: rubikitch
 ;; Maintainer: jidaikobo-shibata
+;; Keywords: dired explorer
+;; Version: 0.2
 ;; for Emacs 24.5.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; [en]
